c57958c2512fb866ab28803ae70511f923cf6859
Alok Kumar <alokkumar012148@gmail.com>
2025-06-21 22:49:20 +0530

    Initial setup, learned basics

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import { View, Text, Button, TouchableOpacity } from "react-native";
+
+const App = () => {
+  return (
+    <View style={{ width: "100%", height: "100%", backgroundColor: "gray", padding: 20 }}>
+      <Text style={{ padding: 20 }}>Hi, I am alok kumar</Text>
+
+      <Button title="press me" />
+      <Button title="press me" />
+      <Button title="press here" />
+
+      <TouchableOpacity style={{ padding: 20, backgroundColor: "green" }}>
+        <Text>Button</Text>
+      </TouchableOpacity>
+
+      
+
+
+    </View>
+  );
+};
+
+export default App;
