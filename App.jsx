86c11617c1b6f441ad151c26bb926b4db84164dd
Alok Kumar <alokkumar012148@gmail.com>
2025-07-16 22:32:59 +0530

    Learning basics of React Native

--- App.jsx
+++ App.jsx
@@ -1,10 +1,21 @@
-import { View, Text, Button, TouchableOpacity } from "react-native";
+import { View, Text } from "react-native";
+import { useState, useEffect } from "react";
 
-const App = () => {
+export default function App () {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
   return (
-    <View style={{ width: "100%", height: "100%", backgroundColor: "gray", padding: 20 }}>
+    <View style={{ width: "100%", height: "100%", backgroundColor: "gray", padding: 20}}>
       <Text style={{ padding: 20 }}>Hi, I am alok kumar</Text>
-
+      <Text style={{ padding: 20 }}>Count: {count}</Text>
+      {/* 
       <Button title="press me" />
       <Button title="press me" />
       <Button title="press here" />
@@ -16,14 +27,8 @@ const App = () => {
         <Text>Button</Text>
       </TouchableOpacity>
       <TouchableOpacity style={{ padding: 20, backgroundColor: "green" }}>
-        <Text>Button</Text>
-      </TouchableOpacity>
-
-      
-
-
+        <Text>Button</Text> 
+      </TouchableOpacity> */}
     </View>
   );
 };
-
-export default App;
